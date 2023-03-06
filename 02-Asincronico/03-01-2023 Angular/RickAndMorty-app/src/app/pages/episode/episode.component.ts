433044fc7269a6550a69c3a42444c8aa607dfd2a
Michael Alvarez <lauryyhorel@gmail.com>
2023-03-06 13:04:09 -0600

    tsEpisode

---
+++ 02-Asincronico/03-01-2023 Angular/RickAndMorty-app/src/app/pages/episode/episode.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { InfoModel } from 'src/app/shared/models/infoModel';
+import { EpisodeTableService } from 'src/app/shared/services/Episode-table.service';
+
+@Component({
+  selector: 'app-episode',
+  templateUrl: './episode.component.html',
+  styleUrls: ['./episode.component.css'],
+})
+export class EpisodeComponent implements OnInit {
+  info: InfoModel;
+
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'air_date',
+    'episode',
+    'url',
+    'created',
+  ];
+  dataSource = [];
+
+  constructor(private episodeSrv: EpisodeTableService) {}
+
+  getEpisode(url: string) {
+    this.episodeSrv.getEpisode(url).subscribe((data: any) => {
+      const { info, results } = data;
+      this.dataSource = results;
+      this.info = info;
+      console.log(this.dataSource);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getEpisode('https://rickandmortyapi.com/api/episode');
+  }
+
+  next(): void {
+    this.getEpisode(this.info.next);
+  }
+
+  preview(): void {
+    this.getEpisode(this.info.prev);
+  }
+}

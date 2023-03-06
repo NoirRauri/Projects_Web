92212a4a79014bb3dd21713ddca1c9b48f2d0146
Michael Alvarez <lauryyhorel@gmail.com>
2023-03-06 10:33:03 -0600

    XD

--- 02-Asincronico/03-01-2023 Angular/RickAndMorty-app/src/app/material.module.ts
+++ 02-Asincronico/03-01-2023 Angular/RickAndMorty-app/src/app/material.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const myListMaterialModule = [
   MatButtonModule,
   MatCardModule,
   MatMenuModule,
-  MatToolbarModule,
   MatIconModule,
   MatGridListModule,
   MatTableModule,
-  MatPaginatorModule
+  MatPaginatorModule,
+  MatToolbarModule,
 ];
 
 @NgModule({

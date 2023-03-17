8bd3979cebdf0fe74bdf868787f737077af5f26f
Michael Alvarez <lauryyhorel@gmail.com>
2023-03-17 17:59:56 -0600

    AddIndexRutaClientes

--- 02-Asincronico/03-01-2023 Angular/loginProject/src/routes/index.ts
+++ 02-Asincronico/03-01-2023 Angular/loginProject/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productos from "./productos";
+import clientes from "./clientes";
 
 const routes = Router();
 
 // ruta para productos
 routes.use('/productos', productos);
+routes.use('/clientes', clientes);
 
 export default routes;

7264639147494578165ca30cf13bbfa8a617c523
Michael Alvarez <lauryyhorel@gmail.com>
2023-03-20 18:38:52 -0600

    addCreate+Udate:Productos

--- 02-Asincronico/03-01-2023 Angular/loginProject/src/controller/ProductosController.ts
+++ 02-Asincronico/03-01-2023 Angular/loginProject/src/controller/ProductosController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 // import { request } from "https";
 import { AppDataSource } from "../data-source";
 import { Productos } from "../entity/Productos";
@@ -19,7 +19,7 @@ class ProductosController {
     }
   }
 
-  // Busquedad get por ID
+  //........................ Buscar producto por ID ........................
   static getById = async (req: Request, res: Response) => {
     //pasar datos por id
     const productosRepo = AppDataSource.getRepository(Productos);
@@ -34,12 +34,84 @@ class ProductosController {
 
     } catch (error) { //si no encuantra el ID manda un msnJson
       return res.status(400).json({ message: 'no se encontro con el id' })
+    }
+  }
+
+  //........................ Creacion de un nuevo producto ........................
+  static create = async (req: Request, res: Response) => {
+
+    const { id, nombre, idCategoria, precio } = req.body;
+
+    // console.log(req.body)
+
+    if (!id) {
+      return res.status(400).json({ mesaage: 'Falta el ID' })
+    } else if (!nombre) {
+      return res.status(400).json({ mesaage: 'Falta el Nombre' })
+    } else if (!idCategoria) {
+      return res.status(400).json({ mesaage: 'Falta la Categoria' })
+    } else if (!precio) {
+      return res.status(400).json({ mesaage: 'Falta el precio' })
+    }
+
+    const productosRepo = AppDataSource.getRepository(Productos);
+
+    if (await productosRepo.findOne({ where: { id } })) {
+      return res.status(400).json({ mesaage: 'El id ya existe' })
+    }
+
+    try {
+
+      let prod = new Productos
+      prod.id = id;
+      prod.nombre = nombre;
+      prod.idCategoria = idCategoria;
+      prod.precio = precio;
+
+      await productosRepo.save(prod);
+      return res.status(201).json({ message: 'El producto fue creado' })
+
+    } catch (error) {
+      return res.status(400).json({ message: 'Los datos estan incompletos o erroneos' })
+    }
+  }
+
+  //........................ Actualizar producto ........................
+  static updateById = async (req: Request, res: Response) => {
+
+    const productosRepo = AppDataSource.getRepository(Productos);
+    const id = parseInt(req.params['id']);
+    // console.log(id)
+    const { nombre, idCategoria, precio } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ mesaage: 'Falta el ID' })
+    } else if (!nombre) {
+      return res.status(400).json({ mesaage: 'Falta el Nombre' })
+    } else if (!idCategoria) {
+      return res.status(400).json({ mesaage: 'Falta la Categoria' })
+    } else if (!precio) {
+      return res.status(400).json({ mesaage: 'Falta el precio' })
+    }
+
+    let prod: Productos
+    try {
+      prod = await productosRepo.findOneOrFail({ where: { id, estado: true } })
+    } catch (error) {
+      return res.status(400).json({ message: 'no se encontro con el id' })
 
     }
 
+    prod.nombre = nombre;
+    prod.idCategoria = idCategoria;
+    prod.precio = precio;
+
+    await productosRepo.save(prod);
+    return res.status(201).json({ message: 'El producto a sido actualizado' })
+
   }
 
-  // Eliminacion logica del producto
+  //........................ Eliminacion logica del producto ........................
   static deleteById = async (req: Request, res: Response) => {
     //pasar datos por id
     const productosRepo = AppDataSource.getRepository(Productos);
